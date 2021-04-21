459ab8e754e27a9a034e5120800c0eb29b22ec78
Sujoy Seal <sujoyseal11@gmail.com>
2021-04-22 01:15:54 +0530

    Uploaded files

---
+++ precache-manifest.2a489e54e8ed1d84362f1b0ec793479a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a21c18e20a1d335e98be1d35f1c1ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "232c9a6160c4aa4503a1",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "5294abb8e5c3a57a3db4",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "232c9a6160c4aa4503a1",
+    "url": "/static/js/2.dc0e9358.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.dc0e9358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5294abb8e5c3a57a3db4",
+    "url": "/static/js/main.9b919038.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
